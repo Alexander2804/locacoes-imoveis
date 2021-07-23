0f8534820890525b7a5a1392640fb5178e812eae
Alexander2804 <67113804+Alexander2804@users.noreply.github.com>
2021-07-23 16:31:09 -0300

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,42 @@
+<! DOCTYPE html >
+< html  lang = " pt-br " >
+< cabeça >
+  < meta  charset = " UTF-8 " >
+  < meta  name = " viewport " content = " width = device-width, initial-scale = 1.0 " >
+  < meta  http-equiv = " Compatível com X-UA " content = " ie = edge " >
+  < title > Locadora </ title >
+  < link  rel = " stylesheet " href = " assets / css / bootstrap.min.css " >
+</ head >
+< corpo >
+  < nav  class = " navbar navbar-expand-lg navbar-light bg-light " >
+    < A  class = " navbar-brand " href =" # " > Locadora </ a >
+    < button  class = " navbar-toggler " type = " button " data-toggle = " collapse " data-target = " #navbarSupportedContent " aria-controls = " navbarSupportedContent "
+      aria-extended = " false " aria-label = " Alternar navegação " >
+      < span  class = " navbar-toggler-icon " > </ span >
+    </ botão >
+    < div  class = " collapse navbar-collapse " id = " navbarSupportedContent " >
+      < ul  class = " navbar-nav mr-imovel " >
+        < li  class = " nav-item active " >
+          < A  class = " nav-link " href =" # " > Locações </ a >  
+          < A  class =" nav-link " href =" /locadora?pagina=app/paginas/locacoes/listagem.php " " > Locações </ a >  
+        </ li >
+        < li  class = " nav-item dropdown " >
+          < a  class = " nav-link dropdown-toggle " href = " # " id = " navbarDropdown " role = " button " data-toggle = " dropdown " aria-haspopup = " true "
+            aria-extended = " false " >
+            Cadastros
+          </ a >
+          < div  class = " dropdown-menu " >
+            < A  class = " suspensa item " href =" /locadora?pagina=app/paginas/carros/listagem.php " > Imovel </ a >
+            < A  class = " suspensa item " href =" /locadora?pagina=app/paginas/clientes/listagem.php " > Clientes </ a >
+            < A  class = " suspensa item " href =" /locadora?pagina=app/paginas/funcionarios/listagem.php " > Funcionários </ a >
+            < div  class = " dropdown-divider " > </ div >
+            < A  class = " suspensa item " href =" /locadora?pagina=app/paginas/cidades/listagem.php " > Cidades </ a >
+            < A  class = " suspensa item " href =" /locadora?pagina=app/paginas/bairros/listagem.php " > Bairros </ a >
+            < div  class = " dropdown-divider " > </ div >
+            
+          </ div >
+        </ li >
+      </ ul >
+    </ div >
+  </ nav >
+  < div  class = " container " >
